780cd12246d99da4d5a9d1c7fad683642d88f630
awjamison <awj228@nyu.edu>
2014-10-23 19:32:21 -0400

    Fixes derivational_family_size().

--- stimuli/lex_dec_vars.py
+++ stimuli/lex_dec_vars.py
@@ -14,6 +14,46 @@ from nltk.corpus import wordnet as wn
 # SUBTLEX, OLD20, AoA, transitivity, denominal/deverbal
 
 
-class LexDecVars(object):
+class LexDecVars(Corpus):
 
-    def __init__(self,
+    def __init__(self, path, item_name='Word', debug=False):
+        super(LexDecVars, self).__init__()  # to inherit data attributes
+        self.read(path, 'exp', item_name)
+        self.words = tuple(w[item_name] for w in self.db['exp'])
+        self.debug = debug
+
+    def derivational_family_size(self, include_multiword=True):
+        # include_multiword: if this option is set to false, only
+        # one-word lemmas are considered. For example, "sleep in" and
+        # "beauty-sleep" are ignored, but "oversleep" and "sleepwalk" are
+        # included. This is really arbitrary. Unfortunately.
+        if 'clx-lemmas' not in self.db:
+            self.read_clx()
+
+        by_morpheme = {}
+
+        for lemma in self.db['clx-lemmas']:
+            if not include_multiword and ('-' in lemma['Head'] or
+            ' ' in lemma['Head']):
+                continue
+            decomposition = lemma['Imm']
+            morphemes = decomposition.split('+')
+            for morpheme in morphemes:
+                if morpheme in self.words:
+                    by_morpheme.setdefault(morpheme, set()).add(lemma['Head'])
+
+        for record in self.db['exp']:
+            decompositions = by_morpheme.get(record['Word'])
+            if decompositions is not None:
+                record['deriv_family_size'] = len(decompositions)
+                if self.debug:
+                    record['deriv_family_size'] = decompositions
+            else:
+                record['deriv_family_size'] = None
+
+# for testing
+def debug():
+    l = LexDecVars(expPath, debug=True)
+
+if __name__ == '__main__':
+    debug()
