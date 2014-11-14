9dee8e8f307820ecff6276f0362c5614a7a765db
Andrew Jamison <awj228@nyu.edu>
2014-11-14 14:28:51 -0500

    Expands number of test options.

--- analysis/reg_cluster.py
+++ analysis/reg_cluster.py
@@ -4,46 +4,40 @@ Created on Aug 18, 2014
 @author: andrew
 '''
 
+import eelbrain
 from eelbrain import save, load, testnd
 
-#eelbrain._set_log_level('debug')
+eelbrain._set_log_level('debug')
 
-dr = '/Volumes/Backup/sufAmb/pickled/'
-IV_alls = ['VerbGivenWord_nocov', 'VerbGivenWord', 'Ambiguity', 'WrdVerbyWeighted', 'WrdBiasWeighted']
-IVs = ['VerbGivenWord', 'Ambiguity']
+def reg_clusters_t_src(mod, IV, factor=None, c1=None, c0=None):
+    """ Cluster permutation (src x time) t-tests on regression coefficients.
+    Example: mod='main-VerbGivenWord', IV='LogFre'
+    """
 
-Y='beta'
-match='subject'
-samples=1000
-pmin=0.04
-tstart=0.13
-tstop=0.45
-mintime=0.03
-minsource=10
-X = 'Type'
+    dr = '/Volumes/Backup/sufAmb/regression/'
+    f = dr+'ols_'+mod+'.pickled'
+    data = load.unpickle(f)
 
-tests = {}
-for v in IVs:
-    print v
-    testsv = {}
-    fil = dr+'ols_'+v+'.pickled'
-    data = load.unpickle(fil)
-    
-    print 'main'
-    testsv['main'] = testnd.ttest_1samp(Y=Y, ds=data.sub("Type=='all'"), match=match, samples=samples, pmin=pmin, tstart=tstart, tstop=tstop, mintime=mintime, minsource=minsource)
-    #===========================================================================
-    # print 'test 1'
-    # testsv['stemVstemS'] = testnd.ttest_rel(Y=Y, ds=data, X=X, c1='stem', c0='stemS', match=match, samples=samples, pmin=pmin, tstart=tstart, tstop=tstop,  mintime=mintime, minsource=minsource)
-    # print 'test 2'
-    # testsv['stemVstemEd'] = testnd.ttest_rel(Y=Y, ds=data, X=X, c1='stem', c0='stemEd', match=match, samples=samples, pmin=pmin, tstart=tstart, tstop=tstop, mintime=mintime, minsource=minsource)
-    # print 'test 3'
-    # testsv['stemSVstemEd'] = testnd.ttest_rel(Y=Y, ds=data, X=X, c1='stemS', c0='stemEd', match=match, samples=samples, pmin=pmin, tstart=tstart, tstop=tstop, mintime=mintime, minsource=minsource)
-    # 
-    #===========================================================================
-    for t in testsv:
-        name = v+'_'+t
-        path = '/Volumes/BackUp/sufAmb/pickled/ols_%s.pickled' % name
-        save.pickle(testsv[t], path)
-    
-    tests[v] = testsv
+    # cluster permutation parameters
+    Y='beta'
+    match='subject'
+    samples=100
+    pmin=0.05
+    tstart=0.13
+    tstop=0.45
+    mintime=0.03
+    minsource=10
 
+    if factor is None:
+        test = testnd.ttest_1samp(Y=Y, ds=data.sub("condition=='main'"), match=match, samples=samples, pmin=pmin,
+                                  tstart=tstart, tstop=tstop, mintime=mintime, minsource=minsource)
+    else:
+        test = testnd.ttest_rel(Y=Y, ds=data, X=factor, c1=c1, c0=c0, match=match, samples=samples, pmin=pmin,
+                                tstart=tstart, tstop=tstop,  mintime=mintime, minsource=minsource)
+
+    print "Finished cluster test: mod=%s, IV=%s, factor=%s, c1=%s, c0=%s" % (mod, IV, factor, c1, c0)
+    path = "/Volumes/BackUp/sufAmb/reg-cluster_time_src/ols_mod-%s_IV-%s_factor-%s_c1-%s_c0-%s.pickled" \
+        % (mod, IV, factor, c1, c0)
+    save.pickle(test, path)
+
+    return test
