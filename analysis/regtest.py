15264f653f02fca55e219de1b30e7d173288ec2a
awjamison <awj228@nyu.edu>
2014-11-14 00:02:00 -0500

    Updates this script for new tests.

--- analysis/regtest.py
+++ analysis/regtest.py
@@ -4,18 +4,24 @@ Created on Aug 17, 2014
 @author: andrew
 '''
 
-from sufAmb6 import e
+from sufamb_experiment import e
 from regression import ols
 from eelbrain import save
 
-IVs = ['Ambiguity', 'WrdVerbyWeighted', 'WrdBiasWeighted']
-covar = ['FamilyOrder', 'LetNum', 'SylNum', 'OrthNeighborhoodFre', 
-         'UnconstrainedBigramFre', 'Log_DerivationalFamilySize', 'LogSynsetNum_Stem',
-         'ParadigmSurprisal', 'LogFre']
+tests = {'stem': [['VerbGivenStem', 'e3SVerbGivenStemS'], ['Ambiguity_Stem', 'Ambiguity_StemS']],
+         'stemS': [['VerbGivenStem', 'e3SVerbGivenStemS'], ['Ambiguity_Stem', 'Ambiguity_StemS']],
+         'main': [['VerbGivenWord']]}
+covar = ['FamilyOrder', 'LetNum', 'SylNum', 'UnconstrainedBigramFre', 'OrthNeighborhoodSize',
+         'DerivationalFamilySize', 'SynsetNum_Stem', 'LogFre', 'ParadigmSurprisal']
 
-for iv in IVs:
-
-    reg = ols(e, iv, covar=covar, fac='Type')
-
-    path = '/Volumes/BackUp/sufAmb/regression/ols_%s.pickled' % iv
-    save.pickle(reg, path)
+for lev in tests:
+    if lev == 'main':
+        factor, level = None, 'all'
+    else:
+        factor, level = 'Type', lev
+    for t in tests[lev]:
+        IVs = covar + tests[lev][t]
+        reg = ols(e, IVs, factor=factor, level=level)
+        tname = '_'.join(tests[lev[t]])
+        path = '/Volumes/BackUp/sufAmb/regression/ols_%s.pickled' % tname
+        save.pickle(reg, path)
