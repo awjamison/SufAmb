244c7fd68d9d2e061b2d99625f8228bea6ea0768
Andrew Jamison <awj228@nyu.edu>
2014-11-13 19:42:53 -0500

    1. Removes old function. 2. Adds ability to handle multiple predictors. 3. Removes option to perform regression on residuals. 4. Minor refactoring to make more readable.

--- analysis/regression.py
+++ analysis/regression.py
@@ -4,89 +4,9 @@ Created on Aug 17, 2014
 @author: andrew
 '''
 
-from collections import defaultdict #@UnresolvedImport
 from eelbrain import Dataset, Factor, morph_source_space, combine
-from numpy.linalg.linalg import LinAlgError #@UnresolvedImport
 
-
-def __ols(e, IV, DV='src', covar=[], fac='Type', ndvar=True, parc='frontal_temporal'):
-    
-    e.set(epoch='epoch_behav_fltr', parc=parc)
-    
-    mri = ['R0053', 'R0606', 'R0632', 'R0684', 'R0734', 'R0845', 'R0850']
-    
-    # check if input parameters are ok
-    if DV != 'src':
-        print "WARNING: untested for DV != 'src'"
-    if not hasattr(covar, '__getitem__'):
-        raise TypeError('covar is of type %s, which has no attribute __getitem__' % type(covar))
-    if not hasattr(covar, '__iter__'):
-        raise TypeError('covar is of type %s, which has no attribute __iter__' % type(covar))     
-    if not ndvar:
-        raise ValueError('ndvar must be True')
-    for s in e:
-        if s in mri: continue
-        e.load_annot() # raises an error if parcellation file not found in fsaverage
-        break
-    
-    betas = defaultdict(list)
-    subjects = Factor([s for s in e], random=True)
-    betas_ds = Dataset( ('subject', subjects), info={'predictor': IV, 'outcome': DV, 'covariates': covar, 'factor':fac} )
-
-    for s in e:
-        print s
-        
-        # avoid IOError if parc only exists in fsaverage; parc will be reset below after loading epochs
-        e.set(parc='')
-        
-        # load src epochs
-        ds = e.load_epochs_stc(sns_baseline=(None, 0), ndvar=ndvar)
-        
-        # create smaller dataset from parcellation
-        if s in mri:
-            ds[DV] = morph_source_space(ds[DV], 'fsaverage')
-        else:
-            ds[DV].source.subject = 'fsaverage'
-        ds[DV].source.set_parc(parc) # reset parc
-        index = ds[DV].source.parc.isnotin(['unknown-lh', 'unknown-rh'])
-        ds[DV] = ds[DV].sub(source=index)
-        
-        # regress main effect
-        if len(covar) > 0:
-            # regress DV on covariate
-            cov = [ds[covar[i]] for i in range(len(covar))]
-            resid = ds[DV].residuals(cov)
-            # regress IV on residuals from previous regression
-            beta = resid.ols(ds[IV])
-        else:
-            beta = ds[DV].ols(ds[IV])
-        betas['all'].append(beta)
-        
-        # regress at each level of fac
-        if fac is not None:
-            levels = ds[fac].cells
-            for l in levels:
-                index = "%s == '%s'" % (fac, l)
-                lev = ds.sub(index)
-                if len(covar) > 0:
-                    # regress DV on covariate
-                    cov = [lev[covar[i]] for i in range(len(covar))]
-                    resid = lev[DV].residuals(cov)
-                    # regress IV on residualevs from previous regression
-                    beta = resid.ols(lev[IV])
-                else:
-                    beta = lev[DV].ols(lev[IV])
-                betas[l].append(beta)
-        
-    for x in betas:
-        betas_ds[x] = combine(betas[x])
-        
-    return betas_ds
-
-
-
-
-def ols(e, IV, DV='src', covar=[], fac='Type', ndvar=True, parc='frontal_temporal'):
+def ols(e, IVs, DV='src', fac='Type', ndvar=True, parc='frontal_temporal'):
     """Currently this only handles a single factor
     """
     
@@ -96,21 +16,27 @@ def ols(e, IV, DV='src', covar=[], fac='Type', ndvar=True, parc='frontal_tempora
     
     # try to catch errors in input arguments early
     if DV != 'src':
-        print "WARNING: untested for DV != 'src'"
-    if not hasattr(covar, '__getitem__'):
-        raise TypeError('covar is of type %s, which has no attribute __getitem__' % type(covar))
-    if not hasattr(covar, '__iter__'):
-        raise TypeError('covar is of type %s, which has no attribute __iter__' % type(covar))     
+        raise NotImplementedError('untested for DV != src.')
     if not ndvar:
-        raise ValueError('ndvar must be True')
+        raise NotImplementedError('untested for ndvar != True.')
+    if not hasattr(IVs, '__getitem__'):
+        raise TypeError('IVs is of type %s, which has no attribute __getitem__.' % type(IVs))
+    if not hasattr(IVs, '__iter__'):
+        raise TypeError('IVs is of type %s, which has no attribute __iter__.' % type(IVs))
+    events = e.load_events()
+    for iv in IVs:
+        if events.has_key(iv) is False:
+            raise ValueError('%s is not in the log file.' % iv)
+    del events
     for s in e:
         if s in mri: continue
-        e.load_annot() # raises an error if parcellation file not found in fsaverage
+        e.load_annot()  # raises an error if parc file not found in fsaverage
         break
-    
+
     # WARNING NOTE: row values are coupled only by the for loop; may use dict in future
     betas = []
-    factor = []
+    vars = []
+    factors = []
     subjects = []
 
     for s in e:
@@ -122,31 +48,23 @@ def ols(e, IV, DV='src', covar=[], fac='Type', ndvar=True, parc='frontal_tempora
         # load src epochs
         ds = e.load_epochs_stc(sns_baseline=(None, 0), ndvar=ndvar)
         
-        # create smaller dataset from parcellation
+        # create smaller dataset that is just the parcellation
         if s in mri:
             ds[DV] = morph_source_space(ds[DV], 'fsaverage')
         else:
             ds[DV].source.subject = 'fsaverage'
-        ds[DV].source.set_parc(parc) # reset parc
-        index = ds[DV].source.parc.isnotin(['unknown-lh', 'unknown-rh'])
+        ds[DV].source.set_parc(parc)  # reset parc
+        index = ds[DV].source.parc.isnotin(['unknown-lh', 'unknown-rh'])  # remove these
         ds[DV] = ds[DV].sub(source=index)
         
         # regress main effect
-        if len(covar) > 0:
-            try:
-                # regress DV on covariate
-                cov = [ds[covar[i]] for i in range(len(covar))]
-                resid = ds[DV].residuals(cov)
-                # regress IV on residuals from previous regression
-                beta = resid.ols(ds[IV])
-            except LinAlgError:
-                raise LinAlgError
-                print "Singular matrix; two or more covariates are collinear."
-                
-        else:
-            beta = ds[DV].ols(ds[IV])
-        factor.append('all')
-        betas.append(beta)
+        mod = tuple(ds[iv] for iv in IVs)
+        b = ds[DV].ols(mod)  # fit multiple ols to predictor vars
+        for name, value in zip(IVs, b):
+            vars.append(name)
+            betas.append(value)
+            factors.append('all')
+            subjects.append(s)
         
         # regress at each level of fac
         if fac is not None:
@@ -154,31 +72,22 @@ def ols(e, IV, DV='src', covar=[], fac='Type', ndvar=True, parc='frontal_tempora
             for l in levels:
                 index = "%s == '%s'" % (fac, l)
                 lev = ds.sub(index)
-                if len(covar) > 0:
-                    try:
-                        # regress DV on covariate
-                        cov = [lev[covar[i]] for i in range(len(covar))]
-                        resid = lev[DV].residuals(cov)
-                        # regress IV on residualevs from previous regression
-                        beta = resid.ols(lev[IV])
-                    except LinAlgError:
-                        raise LinAlgError
-                        print "Singular matrix; two or more covariates are collinear."
-                else:
-                    beta = lev[DV].ols(lev[IV])
-                factor.append(l)
-                betas.append(beta)
+                mod = tuple(lev[iv] for iv in IVs)
+                b = lev[DV].ols(mod)
+                for name, value in zip(IVs, b):
+                    vars.append(name)
+                    betas.append(value)
+                    factors.append(l)
+                    subjects.append(s)
+        else:
+            levels = ()
+        # after each iteration of e, number of rows in betas_ds is increased by (n_levels+1) * n_vars
         
-        facLen = len(levels)+1 # add 1 for main effect (collapsed across levels)
-        subjects.extend( [s]*facLen )
-        # after each iteration of s, number of rows in betas_ds is increased by number of levels in factor
-        
-    betas = combine(betas)
-    factor = Factor(factor, name=fac, random=False)
+    betas = combine(betas)  # combine list of NDVars into single NDVar
+    vars = Factor(vars, name='predictors', random=False)
+    factors = Factor(factors, name='levels', random=False)
     subjects = Factor(subjects, name='subjects', random=True)
-    betas_ds = Dataset( ('subjects',subjects), (fac, factor), ('betas',betas), info={'predictor': IV, 'outcome': DV, 'covariates': covar, 'factor':fac} )
+    betas_ds = Dataset(('subject', subjects), (fac, factors), ('var', vars), ('beta', betas),
+                       info={'predictors': IVs, 'outcome': DV, 'factor': fac})
         
     return betas_ds
-
-
-    
